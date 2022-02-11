46986a2919e074d88d4cbe3964653855ee5a8b25
Daniil <danil.crytit@gmail.com>
2022-02-11 18:28:38 +0300

    create filter

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,3 +1,4 @@
+import AppFilter from '../app-filter/app-filter';
 import AppInfo from '../app-info/app-info';
 import SearchPanel from '../search-panel/search-panel';
 import './app.css';
@@ -8,6 +9,7 @@ function App() {
       <AppInfo />
       <div className="search-panel">
         <SearchPanel />
+        <AppFilter />
       </div>
     </div>
   );
